0449c65caffb1ffaeccfe4a8c69019e065f1f68d
Warisul Imam <md.warisul.imam@g.bracu.ac.bd>
2025-12-12 17:04:03 +0600

    progress: ex.1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,50 +1,20 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
 const Header = ({course}) => (
   <h1>{course}</h1>
 );
 
+const Part = ({part}) => (
+  <p>{part.name} {part.number_of_exercises}</p>
+);
+
 const Content = ({content}) => (
   <>
-    {content.map( (part, index) => (
-      <p key={index}>{part.name} {part.number_of_exercises}</p>
-    ))}
+    {
+      content.map((part, index) => (
+        <Part part={part} key={index} />
+      ))
+    }
   </>
-)
+);
 
 const Total = ({content}) => (
   <p>Number of exercises {content.reduce((accumulator, currentValue) => accumulator + currentValue.number_of_exercises, 0)}</p>
