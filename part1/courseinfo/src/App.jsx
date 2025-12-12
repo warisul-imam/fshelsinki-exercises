72687a2357333b346563994602f39c9dcb7230aa
Warisul Imam <md.warisul.imam@g.bracu.ac.bd>
2025-12-12 16:52:43 +0600

    progress: ex 1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,87 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+const Header = ({course}) => (
+  <h1>{course}</h1>
+);
+
+const Content = ({content}) => (
+  <>
+    {content.map( (part, index) => (
+      <p key={index}>{part.name} {part.number_of_exercises}</p>
+    ))}
+  </>
+)
+
+const Total = ({content}) => (
+  <p>Number of exercises {content.reduce((accumulator, currentValue) => accumulator + currentValue.number_of_exercises, 0)}</p>
+);
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const content = [
+    {
+      "name" : part1,
+      "number_of_exercises": exercises1,
+    },
+    {
+      "name" : part2,
+      "number_of_exercises": exercises2,
+    },
+    {
+      "name" : part3,
+      "number_of_exercises": exercises3,
+    },
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={content} />
+      <Total content={content} />
+    </div>
+  )
+}
+
+export default App
