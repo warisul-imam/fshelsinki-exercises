0f71c7d4ae5dfc3507397e6c081ede1bf22e1b62
Warisul Imam <md.warisul.imam@g.bracu.ac.bd>
2025-12-14 00:11:40 +0600

    progress: ex 1.7-1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,6 +2,22 @@ import { useState } from 'react'
 
 const Button = ({ state, setState, text }) => <button onClick={() => setState(state+1)}>{text}</button>
 
+const Statistics = ({ good, neutral, bad }) => (
+  <>
+    <h1>statistics</h1>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+
+      <br />
+
+      <p>all { good + neutral + bad }</p>
+      <p>average { ( (1)*good + (0)*neutral + (-1)*bad ) / (good + neutral + bad) }</p>
+      <p>positive { 100*good / (good + neutral + bad) } %</p>
+  </>
+);
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -18,11 +34,9 @@ const App = () => {
         <Button state={bad} setState={setBad} text={"bad"} />
       </div>
 
-      <h1>statistics</h1>
-
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      {
+        (good + neutral + bad) != 0 ? <Statistics good={good} neutral={neutral} bad={bad} /> : <p>no feedbacks given</p>
+      }
 
     </div>
   )
