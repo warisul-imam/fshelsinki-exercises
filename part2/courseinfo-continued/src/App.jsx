d49f1c3f581c47ee54c2db2fa70b70bf47929df2
Warisul Imam <md.warisul.imam@g.bracu.ac.bd>
2025-12-14 18:42:05 +0600

    progress: ex 2.5

--- part2/courseinfo-continued/src/App.jsx
+++ part2/courseinfo-continued/src/App.jsx
@@ -1,22 +1,4 @@
-const Header = ({ courseName }) => <h1>{courseName}</h1>
-
-const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
-
-const Total = ({ parts }) => <strong>total of {parts.reduce((accumulator, currentPart) => accumulator + currentPart.exercises, 0)} exercises</strong>
-
-const Course = ({ course }) => (
-  <>
-    <Header courseName={course.name} />
-    {course.parts.map(part =>
-      <Part
-        key={part.id}
-        name={part.name}
-        exercises={part.exercises}
-      />
-    )}
-    <Total parts={course.parts} />
-  </>
-);
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
