25a29bb0407962e59d98424538fd3928d98d775b
Warisul Imam <md.warisul.imam@g.bracu.ac.bd>
2025-12-14 23:49:31 +0600

    progress: ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import {
   Filter,
   AddContact,
@@ -6,13 +7,16 @@ import {
 } from './components'
 
 const App = () => {
-  
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons")
+      .then(response => {
+        const personsData = response.data
+        setPersons(personsData)
+      })
+  }, [persons])
 
   const [nameFilter, setNameFilter] = useState('')
 
