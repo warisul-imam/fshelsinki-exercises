e1308e6656159f730e0c3a80127c5cde70cfb632
Warisul Imam <md.warisul.imam@g.bracu.ac.bd>
2025-12-14 23:38:32 +0600

    progress: add db.json and serve it with json server

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import {
+  Filter,
+  AddContact,
+  Persons
+} from './components'
+
+const App = () => {
+  
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [nameFilter, setNameFilter] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={nameFilter} setFilter={setNameFilter} />
+      <AddContact persons={persons} setPersons={setPersons} />
+      <Persons persons={persons} nameFilter={nameFilter} />
+    </div>
+  )
+}
+
+export default App
