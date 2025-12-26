500731d11ba1bf85c5add1009cf700e5ef48cf2b
waris <waris@debian.laptop>
2025-12-27 00:48:21 +0600

    progress: ex 3.1-3.4

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,62 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+let contacts = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get("/api/persons", (req, res) => {
+   res.json(contacts)
+})
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id
+  const person = contacts.find(person => person.id == id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.get("/info", (req, res) => {
+  let date_time = Date()
+  res.send(`
+    <p>Phonebook has info for ${contacts.length} people</p>
+    <p>${date_time}</p>`
+  )
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id
+  contacts = contacts.filter(person => person.id != id)
+
+  res.status(204).end()
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
