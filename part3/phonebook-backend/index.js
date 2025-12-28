a53c52b1c1ed8299e36ae13f0ac67a00bbe2de79
waris <waris@debian.laptop>
2025-12-28 17:39:02 +0600

    fix-attempt: backend is refusing connections on prod

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const morgan = require("morgan")
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 
 app.use(express.json())
 app.use(express.static('dist'))
